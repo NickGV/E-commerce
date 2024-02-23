a21d6825e2d25db034f546345ff05fd716836370
NickGV <119465477+NickGV@users.noreply.github>
2024-02-22 21:26:47 -0500

    make the project responsive

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -19,81 +19,93 @@ export const CartPage = () => {
   return (
     <>
       {shoppingList.length > 0 ? (
-        <>
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead>
-              <tr>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  NAME
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  PRICE
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  AMOUNT
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  REMOVE
-                </th>
-              </tr>
-            </thead>
-            <tbody className=" divide-y divide-gray-200">
-              {shoppingList.map((item) => (
-                <tr key={item.id}>
-                  <td className="px-6 py-4 whitespace-nowrap flex items-center gap-2">
-                    <img src={item.image} alt="" className="w-14 rounded-lg " />
-                    {item.title}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">{item.price}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="flex items-center">
-                      <button
-                        className="mr-1  text-xl text-white px-2 py-1 "
-                        onClick={() => decreaseAmount(item.id)}
+        <div className="overflow-x-auto">
+          <div className="sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead>
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                       >
-                        -
-                      </button>
-                      <span className="mx-1">{item.amount}</span>
-                      <button
-                        className="ml-1 text-xl text-white px-2 py-1 "
-                        onClick={() => increaseAmount(item.id)}
+                        NAME
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                       >
-                        +
-                      </button>
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <button
-                      className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-300"
-                      onClick={() => removePurchase(item.id)}
-                    >
-                      Eliminar
-                    </button>
-                  </td>
-                </tr>
-              ))}
-              <tr>
-                <td className="px-6 py-4" colSpan="3">
-                  <b>TOTAL:</b>
-                </td>
-                <td className="px-6 py-4">${total()}</td>
-              </tr>
-            </tbody>
-          </table>
+                        PRICE
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        AMOUNT
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        REMOVE
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="divide-y divide-gray-200">
+                    {shoppingList.map((item) => (
+                      <tr key={item.id}>
+                        <td className="px-6 py-4 whitespace-nowrap flex items-center gap-2">
+                          <img
+                            src={item.image}
+                            alt=""
+                            className="w-14 rounded-lg"
+                          />
+                          {item.title}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          {item.price}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                            <button
+                              className="mr-1 text-xl text-white px-2 py-1"
+                              onClick={() => decreaseAmount(item.id)}
+                            >
+                              -
+                            </button>
+                            <span className="mx-1">{item.amount}</span>
+                            <button
+                              className="ml-1 text-xl text-white px-2 py-1"
+                              onClick={() => increaseAmount(item.id)}
+                            >
+                              +
+                            </button>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <button
+                            className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-300"
+                            onClick={() => removePurchase(item.id)}
+                          >
+                            Eliminar
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                    <tr>
+                      <td className="px-6 py-4" colSpan="3">
+                        <b>TOTAL:</b>
+                      </td>
+                      <td className="px-6 py-4">${total()}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
 
-          <div className="mt-4">
+          <div className="mt-4 text-center">
             <button
               className="bg-blue-500 text-white px-4 py-2 rounded-md"
               onClick={handleImpresion}
@@ -102,7 +114,7 @@ export const CartPage = () => {
               Buy
             </button>
           </div>
-        </>
+        </div>
       ) : (
         <div className="flex flex-col items-center justify-center h-screen">
           <div className="text-center">
--- src/pages/StorePage.jsx
+++ src/pages/StorePage.jsx
@@ -19,7 +19,7 @@ export const StorePage = () => {
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold mb-4">Store</h1>
 
-      <section className="grid grid-cols-4 gap-4 p-auto">
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
           <ProductCard
             key={product.id}
